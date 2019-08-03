cd5058ba82c47451179bb9aab1eff18e7dd00090
Вшивков Михаил <vshivkov.ma@gmail.com>
2019-08-03 15:32:40 +0500

    Создание базовой prod конфигурации для webpack

---
+++ webpack.config.prod.js
@@ -0,0 +1,38 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+  entry: ['react-hot-loader/patch', './src/index.js'],
+  resolve: {
+    extensions: ['.jsx', '.js', 'css'],
+    alias: {
+      'react-dom': '@hot-loader/react-dom'
+    }
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].[contenthash:8].js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader'
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      },
+      {
+        test: /\.(svg|png|jpg|woff2)$/,
+        loader: 'file-loader'
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html'
+    })
+  ]
+};
