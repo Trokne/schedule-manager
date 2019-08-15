b6c434cfea0228bd97c3d3e027db31af5fd52e4b
Вшивков Михаил <vshivkov.ma@gmail.com>
2019-08-15 17:56:52 +0500

    Подключение redux к хедер-меню

--- src/reducers/user.js
+++ src/reducers/user.js
@@ -1,10 +1,8 @@
-const initialState = [
-  {
-    name: 'Безымянный'
-  }
-];
+const initialState = [{ isLogin: false }];
 
 export default function user(state = initialState, action) {
+  if (action === undefined || action === null) return state;
+
   switch (action.type) {
     default:
       return state;
