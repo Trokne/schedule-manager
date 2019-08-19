79609d1c604715ca6b7f1fc10f64779f0a12c4c4
Вшивков Михаил <vshivkov.ma@gmail.com>
2019-08-19 17:10:28 +0500

    Инпуты на странице входа

--- src/components/auth/tabs/login.jsx
+++ src/components/auth/tabs/login.jsx
@@ -1,7 +1,9 @@
 import React, { Fragment, Component } from 'react';
 import PropTypes from 'prop-types';
 import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
 import '../../../styles/authpage.pcss';
+import '../../../styles/logintab.pcss';
 
 class Login extends Component {
   componentDidMount() {
@@ -16,11 +18,19 @@ class Login extends Component {
           className="auth-input"
           required
           id="outlined-required"
-          label="Еееее"
-          defaultValue="Hello World"
-          margin="normal"
+          label="Логин"
           variant="outlined"
         />
+        <TextField
+          className="auth-input"
+          required
+          id="outlined-required"
+          label="Пароль"
+          variant="outlined"
+        />
+        <Button variant="contained" className="auth-input button" color="primary" to="/auth">
+          Войти
+        </Button>
       </Fragment>
     );
   }
